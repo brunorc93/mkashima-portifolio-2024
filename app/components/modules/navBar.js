e81dd845c067ce3f852578ed221f85439885fa59
BrunORC <brunorcdev@gmail.com>
2024-06-10 20:51:25 +0200

    fixing formatting on navBar.js

--- app/components/modules/navBar.js
+++ app/components/modules/navBar.js
@@ -19,16 +19,13 @@ export default function Navbar() {
                             <br></br>
                             <br></br>
                             <li className={navbar.navbaritem}><Link href="/fotografia"><a>/ FOTOGRAFIA</a></Link></li>
-                            <li className={navbar.navbaritem}><Link href="/webDev"><a>/ WEB DEV</a></Link></li>
-                            <li className={navbar.navbaritem}><Link href="/info"><a>/ INFO</a></Link></li>
-                            <li className={navbar.navbaritem}><Link href="/contato"><a>/ CONTATO</a></Link></li>
                             <br></br>
                             <br></br>
                             <br></br>
                             <ul className={navbar.navbarsociallist}>
                                     <li className={navbar.navbarsocialitem}><a target="_blank" href="https://github.com/mkashima-studio" rel="noopener noreferrer"><img src="/circle-green.svg" alt= "enabled" width="5px"/> GITHUB</a></li>
                                     <li className={navbar.navbarsocialitem}><a target="_blank" href="https://www.instagram.com/mkashima" rel="noopener noreferrer"><img src="/circle-red.svg" alt= "disabled" width="5px"/> INSTAGRAM</a></li>
-                                    <li className={navbar.navbarsocialitem}><a target="_blank" href="" rel="noopener noreferrer"><img src="/circle-red.svg" alt= "disabled" width="5px"/> BLOG</a></li>
+                                    <li className={navbar.navbarsocialitem}><a target="_blank" href="" rel="noopener noreferrer"><img src="/circle-red.svg" alt= "disabled" width="5px"/> BLOG</a></li>                            
                             </ul>
                         </ul>        
                     </div>  
